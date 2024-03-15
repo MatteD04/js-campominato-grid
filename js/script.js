e896a7ebcf539176eaa929c18580db26b6a1d6ad
MatteD04 <matteo.dantonio04@gmail.com>
2024-03-15 17:47:57 +0100

    feat: add javascript to project

--- js/script.js
+++ js/script.js
@@ -4,3 +4,33 @@
 //Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro 
 //ed emetto un messaggio in console con il numero della cella cliccata.
 
+//al click sul bottone genero la griglia
+const btnPlay = document.querySelector('#btn');
+btnPlay.addEventListener('click', function() {
+    for(let i = 1; i <= 100; i++) {
+        const newSquare = generateSquare(i);
+        mainGrid.append(newSquare);
+    }
+})
+
+// per 100 volte creare un quadrato che aggiungo a grid
+const mainGrid = document.querySelector('#grid');
+console.log(mainGrid);
+
+    
+// Funzione che genera un quadrato
+// number -> rappresenta il numero
+// return: elemento del dom che rappresenta un quadrato
+function generateSquare(number) {
+    const newSquare = document.createElement('div');
+    newSquare.classList.add('square');
+    newSquare.innerHTML = `<span>${number}</span>`;
+    
+    //click su ogni quadrato
+    newSquare.addEventListener('click', function() {
+            
+        this.classList.add('clicked');
+        console.log(number);
+    });
+    return newSquare;
+}
